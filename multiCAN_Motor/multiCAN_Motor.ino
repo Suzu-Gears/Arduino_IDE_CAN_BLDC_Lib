d1072aeeafce0569d358d7d42cf2e30c184e72fb
Suzu-Gears <47468734+Suzu-Gears@users.noreply.github.com>
2025-11-12 23:16:17 +0900

    DAMIAOとロボマス共存

---
+++ multiCAN_Motor/multiCAN_Motor.ino
@@ -0,0 +1,84 @@
+#include <RP2040PIO_CAN.h>
+
+#include "C6x0.h"
+#include "CANManager.h"
+#include "DM2325.h"
+
+const uint32_t CAN_TX_PIN = 0;
+const uint32_t CAN_RX_PIN = 1;
+
+const uint32_t dm_master = 0; 
+const uint32_t dm_slave = 9;  //DMモーターはSlaveID1の場合、MITモードの指令のIDが0x201となりロボマスモーターのID1からのフィードバックと被るので、共存させるなら9より大きいIDを使う
+
+// Single hub reading the physical CAN
+CANHub canHub(&CAN);
+
+C6x0 c6x0;
+// DM motor example placeholders
+CANClient* dm_client = nullptr;
+DM::Motor* dm_motor = nullptr;
+
+void setup() {
+  Serial.begin(115200);
+
+  CAN.setTX(CAN_TX_PIN);
+  CAN.setRX(CAN_RX_PIN);
+  CAN.begin(CanBitRate::BR_1000k);
+
+  // Create a client for the C6x0 feedback IDs 0x201..0x201+8
+  CANClient* c6x0_client = canHub.createClientWithRange(0x201, 8);
+  c6x0.setCAN(c6x0_client);
+
+  dm_client = canHub.createClientWithIds({ dm_master });
+  // create motor instance dynamically so we can pass the client pointer
+  dm_motor = new DM::Motor(dm_client, dm_master, dm_slave, DM::DM_ControlMode::DM_CM_VEL);
+  // initialize and enable
+  dm_motor->initialize();
+  dm_motor->enable();
+}
+
+void loop() {
+  c6x0.update();
+  if (dm_client) {
+    // poll hub for DM messages as well
+    // dm_client->available() will cause canHub.poll() internally
+    dm_client->available();
+    // send position 0 with zero velocity limit
+    dm_motor->update();
+  }
+
+  dm_motor->sendVelocityRPS(0.1f);
+
+  float kp = 100;
+  float rps = c6x0.getRpm(C610_ID_1) / 60.0f;
+  float rps_ref = 10;
+  float current_ref = kp * (rps_ref - rps);
+
+  c6x0.setCurrent(C610_ID_1, current_ref);
+
+  c6x0.transmit();
+
+  Serial.print("ID: 0x");
+  Serial.print(0x201 + C610_ID_1, HEX);
+  Serial.print(", Angle: ");
+  Serial.print(c6x0.getPosition(C610_ID_1));
+  Serial.print(" deg, RPS: ");
+  Serial.print(rps);
+  Serial.print(", Command_mA: ");
+  Serial.print(current_ref);
+  Serial.print(", Actual_mA: ");
+  Serial.print(c6x0.getCurrent(C610_ID_1));
+  if (dm_motor) {
+    Serial.print("\t DM_Status: ");
+    Serial.print(dm_motor->getStatus());
+    Serial.print(", DM_Pos_rad: ");
+    Serial.print(dm_motor->getPosition());
+    Serial.print(", DM_Vel_rad_s: ");
+    Serial.print(dm_motor->getVelocity());
+    Serial.print(", DM_Torque: ");
+    Serial.print(dm_motor->getTorque());
+  }
+  Serial.println();
+
+  delay(1);
+}
